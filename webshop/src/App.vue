463da64c6563f2c8c24427206d123141bd37dbe1
Cecilie94 <144773754+Cecilie94@users.noreply.github.com>
2024-03-05 13:06:20 +0100

    routes work now

--- webshop/src/App.vue
+++ webshop/src/App.vue
@@ -1,14 +1,9 @@
 <script setup>
-import ProductList from "./views/ProductList.vue";
-import SingleProduct from "./views/SingleProduct.vue";
-import { useCounterStore } from "@/stores/counter";
-
-const counter = useCounterStore();
+import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-  <ProductList />
-
-  <SingleProduct />
-
+  <div class="app-container">
+    <RouterView />
+  </div>
 </template>
