9ce34d2c0f705be40ee64aa05ffb2243510cf06a
Ezra Andersen <stardustezra@gmail.com>
2024-03-05 10:19:35 +0100

    counter

--- webshop/src/App.vue
+++ webshop/src/App.vue
@@ -1,23 +1,11 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { useCounterStore } from "@/stores/counter";
+
+const counter = useCounterStore();
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <button @click="counter.increment()">Increment</button>
 </template>
 
 <style scoped>
