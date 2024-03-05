e1eb22b016dfbc87eea36b5ea0ba1b36db625654
Sara Gylden <saragbusch@live.dk>
2024-03-05 12:07:39 +0100

    Produkt liste

--- webshop/src/views/ProductList.vue
+++ webshop/src/views/ProductList.vue
@@ -3,14 +3,29 @@
 </script>
 
 <template>
-<div class="container">
-  <div class="product-list">
-    <h2>Product List</h2>
-  <div v-for="product in products" :key=" product.id" class="product">
-    <p>{{ product.name }}</p>
-    <p>{{ product.price }}</p>
-    <button @click="addtoCard(product)">Add to Cart</button>
+  <div class="container">
+    <div class="product-list">
+      <h2>Product List</h2>
+      <div v-for="product in products" :key="product.id" class="product">
+        <p>{{ product.name }}</p>
+        <p>{{ product.price }}</p>
+        <button @click="addToCart(product)">Add to Cart</button>
+      </div>
     </div>
   </div>
-</div>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+const products = ref([
+  { id: 1, name: 'Nike Dunk Panda', price: 10 },
+  { id: 2, name: 'Nike Jordan 1', price: 20 },
+  { id: 3, name: 'Sketcher', price: 30 }
+]);
+
+const addToCart = (product) => {
+  console.log('Adding product to cart:', product);
+  // Add your logic to add the product to the cart here
+};
+</script>
