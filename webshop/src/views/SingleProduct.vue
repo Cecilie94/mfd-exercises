293e7b270e6395af4e981732e9b2c6e667a4e4bc
Sara Gylden <saragbusch@live.dk>
2024-03-05 14:24:10 +0100

    tilføjet nogle funktioner
    Gjort sådan at når man har tilføjet noget i cart, så kan man fjerne det igen (opdateres dog først når man har gået tilbage til landingpage og ind på "product #" igen - !! dette skal fikses så man ikke behøver opdatere!!)

--- webshop/src/views/SingleProduct.vue
+++ webshop/src/views/SingleProduct.vue
@@ -1,12 +1,13 @@
 <template>
-  <div>
-    <h1>{{ product ? product.name : '' }}</h1>
-    <p>{{ product ? product.description : '' }}</p>
+  <div v-if="product">
+    <h1>{{ product.name }}</h1>
+    <p>{{ product.description }}</p>
     <h2>hello</h2>
+    <button @click="addToCart" :disabled="isProductInCart(product.id)">{{ isProductInCart(product.id) ? 'Already in Cart' : 'Add to Cart' }}</button>
+    <button @click="removeFromCart" v-if="isProductInCart(product.id)">Remove from Cart</button>
     <router-link to="/" class="back-button">Go back</router-link>
   </div>
 </template>
-
 
 <script setup>
 import { ref, onMounted } from "vue";
@@ -28,7 +29,38 @@ const products = ref([
   { id: 3, name: "Sketcher", description: "Description for Sketcher" },
 ]);
 
-// Filtrer produktet baseret på id
+function addToCart() {
+  // Funktion til at tilføje et produkt til kurven
+  if (product.value) { // Kontroller om produktet er defineret
+    const cartItems = JSON.parse(localStorage.getItem('cart')) || [];
+    // Kontroller om produktet allerede er i kurven
+    if (!isProductInCart(product.value.id)) { 
+      cartItems.push(product.value); // Tilføj produktet til kurven
+      localStorage.setItem('cart', JSON.stringify(cartItems)); // Opdater cart i localStorage
+    }
+  }
+}
+
+function removeFromCart() {
+  // Funktion til at fjerne et produkt fra kurven
+  if (product.value) { 
+    let cartItems = JSON.parse(localStorage.getItem('cart')) || []; 
+    // Fjerner produktet fra kurven baseret på produktets id
+    cartItems = cartItems.filter(item => item.id !== product.value.id); 
+    localStorage.setItem('cart', JSON.stringify(cartItems));
+    // Nulstil isProductInCart til false, så "Add to Cart" knappen bliver tilgængelig igen
+    isProductInCart.value = false; 
+  }
+}
+
+function isProductInCart(productId) {
+  // Funktion til at kontrollere om et produkt er i kurven
+  const cartItems = JSON.parse(localStorage.getItem('cart')) || []; 
+  // Kontroller om kurven indeholder produktet baseret på produktets id
+  return cartItems.some(item => item.id === productId); 
+}
+
+
 const product = ref(null);
 
 onMounted(() => {
