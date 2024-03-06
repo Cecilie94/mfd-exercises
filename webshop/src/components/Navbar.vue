c46d441af56750224a84b7e910b249a2969cf548
Cecilie94 <144773754+Cecilie94@users.noreply.github.com>
2024-03-06 09:09:38 +0100

    intgrated fontawesome and added a cart icon to our cart

--- webshop/src/components/Navbar.vue
+++ webshop/src/components/Navbar.vue
@@ -18,12 +18,16 @@ export default defineComponent({
 <template>
   <nav>
     <ul>
-      <li><router-link to="/">Home</router-link></li>
-      <li><router-link to="/products">Products</router-link></li>
+      <li><router-link to="/">Products</router-link></li>
       <li><router-link to="/about">About</router-link></li>
-      <li><router-link to="/contact">Contact</router-link></li>
       <li>
-        <router-link to="/cart">Cart ({{ cartItemCount }})</router-link>
+        <router-link to="/contact">Contact</router-link>
+      </li>
+      <li>
+        <router-link to="/cart"
+          >Cart ({{ cartItemCount }})
+          <font-awesome-icon class="fa-cs" icon="fa-solid fa-cart-shopping"
+        /></router-link>
       </li>
     </ul>
   </nav>
--- webshop/src/main.js
+++ webshop/src/main.js
@@ -1,14 +1,30 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+import "@fortawesome/fontawesome-free/css/all.css";
 
-app.use(createPinia())
-app.use(router)
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
 
-app.mount('#app')
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* import specific icons */
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { faComment } from "@fortawesome/free-solid-svg-icons";
+
+/* add icons to the library */
+library.add(faCartShopping, faComment);
+
+const app = createApp(App);
+
+app.component("font-awesome-icon", FontAwesomeIcon);
+app.use(createPinia());
+app.use(router);
+
+app.mount("#app");
